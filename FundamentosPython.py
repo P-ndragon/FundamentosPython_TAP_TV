70ae65127fa277529f4f3164a8eb460885f357f9
Pendragon <j.luna32@outlook.es>
2023-02-14 16:43:09 -0600

    Primer commit

---
+++ FundamentosPython.py
@@ -0,0 +1,26 @@
+def nuevoTema(tema):
+ 	print ("=========",tema, "==========")
+
+#Esto es un comentario
+if __name__ == "__main__":
+	nuevoTema("Operadores aritmeticos")
+	print ("Operador de division entera (10//3) :", 10//3)
+	print ("Operador de potencias (5**5):", 5**5) 
+	print ("Operador de cambio de signo (-5) :", -5)
+
+	print ("======Logicos======")
+	print ("Operador and (True and true): ", True and True)
+	print ("Operador and (True and False): ", True and False)
+	print ("Operador and (False and False): ", False and False)
+	print ("Operador not (not False): ", not False)
+	print ("Operador not (not True): ", not True)
+	print ("Operador or (True or True): ", True or True)
+	print ("Operador or (True or False) ", True or False)
+	print ("Operador or (False or True) ", False or True)
+	print ("Operador or (False or False) ", False or True)
+	
+
+	print ("==== Operadores de comparacion =======")
+	
+	print ("== es igual", 5==5)
+	print ("!= distinto a", 3!=4)
