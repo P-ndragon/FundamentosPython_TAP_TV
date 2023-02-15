ebdd0ed78fac87213800834a5eb02ee750b11170
Pendragon <j.luna32@outlook.es>
2023-02-15 16:50:22 -0600

    commit 3

--- FundamentosPython.py
+++ FundamentosPython.py
@@ -38,3 +38,55 @@ if __name__ == "__main__":
 	print (z,type(z))
 	print (h,type (h))
 
+	nuevoTema("Flotantes")
+
+	x = 1297.5
+	print(x, type(x))
+	y = 0.052829
+	print(y, type (y))
+
+	nuevoTema("Numeros complejos")
+	x = -46j
+	y = 12 + 45j
+	z = 2j
+	print(x, type(x))
+	print (y, type(y))
+	print(z, type(z))
+
+	nuevoTema("Listas")
+
+	a = [10, 30.5, "Python List"]
+	print (a)
+	a = ["Lista2", 45, 16.3j]
+	print(a)
+	print(a[2])
+	a[1] = 34.6
+	print(a)
+
+	nuevoTema("Tuplas")
+	t = (25,'tuple', 1+10j)
+	print("t[1] =", t[1])
+	print(("t^[0:3] = ", t[0:3]))
+	print(t) 
+
+	nuevoTema("Conjuntos")
+	c = {50, 20, 10, 4, 8 ,50 } #en un conjunto no repite datos
+	print(c)
+
+	nuevoTema("Diccionarios")
+	d = {1:'val1', "2":45}
+	print(d,type(d))
+	print(d["2"])
+	print(d[1])
+
+	nuevoTema("Cadenas")
+	s = "Esta es una linea simple"
+	print(s)
+	
+	c2 = 'Cadena entre comillas sencillas'
+	print(c2)
+	c3 = ''' 
+	Cadena
+		de varias 
+	Lines'''
+	print(c3)
